2fe731be355cc6993718025a88d438a97a9234ca
Oskar Paulsson <oskar.paulsson@afry.com>
2025-08-22 09:58:05 +0200

    update git hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 3dde23c3dbf59f49dbcbedbee220bd9a52161e95
+AESD_ASSIGNMENTS_VERSION = 3d876c9f6ef15e027359a3634dfbfc08ece50bcd0
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,8 +20,8 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/finder-app $(TARGET_DIR)/etc/finder-app
-	$(INSTALL) -D -m 0755 $(@D)/conf $(TARGET_DIR)/etc/finder-app/conf
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/* $(TARGET_DIR)/etc/finder-app
+	$(INSTALL) -D -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/conf
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
