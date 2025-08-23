4dd50d6d910b74d831566d0a7bb4750c1541f546
Oskar Paulsson <oskar.paulsson@afry.com>
2025-08-23 21:35:01 +0200

    fix?

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3d876c9f6ef15e027359a3634dfbfc08ece50bcd0'
+AESD_ASSIGNMENTS_VERSION = 3d876c9f6ef15e027359a3634dfbfc08ece50bcd0
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-oskpau.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-oskpau.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
